fcdf0d1478f300b1c2cf34787817f6f5d6caec51
002-umulkher <umulkherjabir@gmail.com>
2026-03-16 14:02:31 +0300

    update

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ fetch('posts.json')
     data.forEach((post) => {
       const card = `
         <article class="card">
-          <div class="card-image" style="background-color: ${post.imageColor};">
+          <div class="card-image" style="background-color: ${post.imageColor}0%, #ffffff 100%);">
           ${post.icon || '🏥'}
           </div>
           <div class="card-content">
